6417c67c1d28323aed574af655b10690b1af3573
pulkit31s <youremail@example.com>
2025-09-14 20:25:16 +0530

    error solved

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -14,11 +15,19 @@ const Footer = () => {
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h3 className="text-xl font-semibold mb-2">Quick Links</h3>
             <ul>
-              <li><a href="/" className="text-gray-400 hover:text-white">Home</a></li>
-              <li><a href="/events" className="text-gray-400 hover:text-white">Events</a></li>
-              <li><a href="/#about" className="text-gray-400 hover:text-white">About</a></li>
-              <li><a href="/#contact" className="text-gray-400 hover:text-white">Contact</a></li>
-            </ul>
+              <li>
+  <Link href="/" className="text-gray-400 hover:text-white">Home</Link>
+</li>
+<li>
+  <Link href="/events" className="text-gray-400 hover:text-white">Events</Link>
+</li>
+<li>
+  <Link href="#about" className="text-gray-400 hover:text-white">About</Link>
+</li>
+<li>
+  <Link href="#contact" className="text-gray-400 hover:text-white">Contact</Link>
+</li>
+</ul>
           </div>
           <div className="w-full md:w-1/3">
             <h3 className="text-xl font-semibold mb-2">Follow Us</h3>
--- src/components/time-line-demo.tsx
+++ src/components/time-line-demo.tsx
@@ -88,62 +88,63 @@ export function TimelineDemo() {
         </div>
       ),
     },
-    {
-      title: "GitHub Workshop",
-      content: (
-        <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            This workshop is designed to introduce participants to the fundamentals of Git and GitHub, essential tools for modern software development and version control. Whether you are a beginner or have some experience with version control, this workshop will help you understand and use these tools effectively in collaborative and personal projects.
-            </p>
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmuSNOvW5sXqBcL5JZWSwTAVrjKlC8Px26vYk9"
-              alt="Workshop"
-              width={500}
-              height={500}
-              unoptimized
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmM4HtZoNTW7KPBDzuvxaNGEL3A8bHqI2wR5Ui"
-              alt="Participants"
-              width={500}
-              height={500}
-              unoptimized
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-          </div>
-        </div>
-      ),
-    },
-    {
-      title: "FunWin",
-      content: (
-        <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            This event  is the ultimate game challenge where participants has to compete through 3 exciting rounds of mind-bending puzzles and games. It can be played in team of 1-4 and the teams will be selected based on the elimination.
-            </p>
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmDKCYcN56Sn7vXwHKGguFEtJh8mY4oV0iqA1s"
-              alt="Workshop"
-              width={500}
-              height={500}
-              unoptimized
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmmNEz9eOXKrGinVW30XZcqoDxLIt4s2bPplRO"
-              alt="Participants"
-              width={500}
-              height={500}
-              unoptimized
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-          </div>
-        </div>
-      ),
-    },
+    // {
+    //   title: "GitHub Workshop",
+    //   id:4,
+    //   content: (
+    //     <div>
+    //       <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+    //         This workshop is designed to introduce participants to the fundamentals of Git and GitHub, essential tools for modern software development and version control. Whether you are a beginner or have some experience with version control, this workshop will help you understand and use these tools effectively in collaborative and personal projects.
+    //         </p>
+    //       <div className="grid grid-cols-2 gap-4">
+    //         <Image
+    //           src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmuSNOvW5sXqBcL5JZWSwTAVrjKlC8Px26vYk9"
+    //           alt="Workshop"
+    //           width={500}
+    //           height={500}
+    //           unoptimized
+    //           className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+    //         />
+    //         <Image
+    //           src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmM4HtZoNTW7KPBDzuvxaNGEL3A8bHqI2wR5Ui"
+    //           alt="Participants"
+    //           width={500}
+    //           height={500}
+    //           unoptimized
+    //           className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+    //         />
+    //       </div>
+    //     </div>
+    //   ),
+    // },
+    // {
+    //   title: "FunWin",
+    //   content: (
+    //     <div>
+    //       <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+    //         This event  is the ultimate game challenge where participants has to compete through 3 exciting rounds of mind-bending puzzles and games. It can be played in team of 1-4 and the teams will be selected based on the elimination.
+    //         </p>
+    //       <div className="grid grid-cols-2 gap-4">
+    //         <Image
+    //           src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmDKCYcN56Sn7vXwHKGguFEtJh8mY4oV0iqA1s"
+    //           alt="Workshop"
+    //           width={500}
+    //           height={500}
+    //           unoptimized
+    //           className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+    //         />
+    //         <Image
+    //           src="https://w61l8n764j.ufs.sh/f/9QpvlA7ofhDmmNEz9eOXKrGinVW30XZcqoDxLIt4s2bPplRO"
+    //           alt="Participants"
+    //           width={500}
+    //           height={500}
+    //           unoptimized
+    //           className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+    //         />
+    //       </div>
+    //     </div>
+    //   ),
+    // },
     // {
     //   title: "Recent Activities - 2025",
     //   content: (
@@ -187,7 +188,12 @@ export function TimelineDemo() {
   ];
   return (
     <div className="w-full">
-      <Timeline data={data} />
+      <Timeline
+        data={data.map((item) => ({
+          ...item,
+          key: item.title, // ✅ ensures each child has a unique key
+        }))}
+      />
     </div>
   );
 }
